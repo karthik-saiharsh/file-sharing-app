50926dbe6d40c8fe8bf9be3c20812646b3977e54
Karthik Saiharsh <ks.the.visionary@gmail.com>
2025-05-13 07:13:33 +0530

    Fixed an issue with deployment

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,5 @@
-import type { NextConfig } from "next";
-
 const nextConfig = {
-  webpack: (config, options) => {
-      config.module.rules.push({
-        test: /\.node/,
-        use: 'node-loader'
-      })
-   
-      return config
-    },
+
 };
 
 export default nextConfig;
