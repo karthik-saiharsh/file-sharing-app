5002ef17011a5b4ed965ccd33148c0b49a7819ff
Karthik Saiharsh <ks.the.visionary@gmail.com>
2025-05-15 19:14:00 +0530

    Moved the file upload api to a different service

--- lib/backend.ts
+++ lib/backend.ts
@@ -1,61 +1,14 @@
 "use server";
 
-import { Client, GatewayIntentBits, TextChannel } from "discord.js";
 import { neon } from "@neondatabase/serverless";
 import { createHash } from "crypto";
 import { blake3 } from "hash-wasm";
 import crypto from "crypto"
 
 ///// Global Variables /////
-let botInitialised = false;
-let client: Client;
+const sql = neon(`${process.env.DATABASE_URL}`);
 
 //// Helper Functions
-async function initBot(): Promise<void> {
-  if (botInitialised) return;
-
-  client = new Client({
-    intents: [
-      GatewayIntentBits.Guilds,
-      GatewayIntentBits.GuildMessages,
-      GatewayIntentBits.MessageContent,
-    ],
-  });
-
-  return new Promise((resolve, reject) => {
-    client.once("ready", () => {
-      botInitialised = true;
-      console.log("Bot initialised Successfully!");
-      resolve();
-    });
-
-    client.login(`${process.env.DISCORD_TOKEN}`).catch(reject);
-  });
-}
-
-async function sendData(message: string, file: Buffer, fileName: string) {
-  if (!botInitialised) {
-    await initBot();
-  }
-
-  const channel = (await client.channels.cache.get(`${process.env.DISCORD_CHANNEL_ID}`)) as TextChannel;
-
-  try {
-    if (channel && channel.isTextBased()) {
-      const messageSent = await channel.send({
-        content: message,
-        files: [{ attachment: file, name: fileName }],
-      });
-      return messageSent.id;
-    }
-  } catch (e) {
-    console.log(e);
-    return "fail";
-  } finally {
-    client.destroy();
-  }
-}
-
 function generateFileID(): string {
   const alphas = "abcdefghijklmnopqrstuvwxyz";
   const nums = "0123456789";
@@ -63,7 +16,7 @@ function generateFileID(): string {
 
   let fileID = "";
 
-  while (fileID.length < 5) {
+  while (fileID.length < 4) {
     fileID += chars[Math.floor(Math.random() * 62) % 62];
   }
   return fileID;
@@ -73,111 +26,91 @@ function getSHA256(str: string) {
   return `${createHash("sha256").update(str).digest("hex")}`;
 }
 
-async function fetchFileURL(msgID: string) {
-  if (!botInitialised) {
-    await initBot();
-  }
-
-  const channel = (await client.channels.cache.get(`${process.env.DISCORD_CHANNEL_ID}`)) as TextChannel;
-
-  const message = await channel.messages.fetch(msgID);
-  const file_url = await message.attachments.first()?.url || "";
-  const file_name = await message.attachments.first()?.name || "";
-  const file_contentType = await message.attachments.first()?.contentType || "";
-  client.destroy();
-  return {url: file_url, name: file_name, type: file_contentType};
-}
-
 async function fetchMessageIDandKEYSum(fileID: string) {
-  const sql = neon(`${process.env.DATABASE_URL}`);
   const result = await sql`select msgID, decKey from filesharingapp where fileID=${fileID}`;
   let retvalue: Record<string, any> = {};
   result.forEach(val => retvalue = val);
   return retvalue;
 }
 
-async function addDataDB(file: Buffer, fileName: string, key: string) {
-  const sql = neon(`${process.env.DATABASE_URL}`);
-
+// Main exposed Functions //
+export async function uploadFile(data: FormData) {
   // create a new fileID
   let fileId = generateFileID();
 
-  const result =
-    await sql`SELECT EXISTS (SELECT 1 FROM filesharingapp WHERE fileID = ${fileId})`;
+  // Check if the file id already exists
+  const result = await sql`SELECT EXISTS (SELECT 1 FROM filesharingapp WHERE fileID = ${fileId})`;
 
-  if (!result[0].exists) {
-    try {
-      const msgStat = await sendData(fileId, file, fileName);
-      if (msgStat != "fail") {
-        await sql`insert into filesharingapp values(${fileId}, ${msgStat}, ${getSHA256(key)})`;
-        return fileId;
+  if (result[0].exists) {
+    // If file exists, try with a new fileID
+    return await uploadFile(data);
+  } 
+  else {
+    const file: File | null = data.get('file') as unknown as File;
+    if(!file) return "fail"
+    const key = data.get('key') as string;
+
+    const dataToSend = new FormData();
+    dataToSend.set("filedata", file);
+    dataToSend.set("message", fileId);
+    dataToSend.set("key", key);
+
+    // Upload file to Discord server after encrypting it
+    const response = await fetch(`${process.env.DB_BOT_URL}/api/upload`,
+       {
+        method: 'POST',
+        body: dataToSend
+       });
+
+      if(response.ok)  {
+        const response_json = await response.json();
+        const msgID = response_json.msgid;
+
+        // Upload the metadata to database
+        await sql`insert into filesharingapp values(${fileId}, ${msgID}, ${getSHA256(key)})`;
+
+        return {status: "success", fileid: fileId};
       }
-    } catch (_) {
-      return false;
-    }
-  } else {
-    return addDataDB(file, fileName, key);
-  }
+      else return "fail";
+  }  
 }
 
-async function getOriginalFileURL(fileId: string, key: string) {
-  const file_metadata = await fetchMessageIDandKEYSum(fileId);
-  const keysum = getSHA256(key);
 
-  if(keysum != file_metadata.deckey) {
+// Download the file from the database
+export async function downloadFile(data: FormData) {
+  const fileID = data.get("fileid") as string;
+  const key_entered = data.get("key") as string;
+  const key_entered_sum = getSHA256(key_entered);
+  
+
+  const stored_file_info = await fetchMessageIDandKEYSum(fileID);
+
+
+  if(key_entered_sum != stored_file_info.deckey) {
     return "fail";
   }
 
-  const fileData = await fetchFileURL(file_metadata.msgid);
-  return fileData;
-}
+  const dataToSend = new FormData()
+  dataToSend.set("msgid", stored_file_info.msgid)
 
-// Main exposed Functions //
-export async function uploadFile(data: FormData) {
-  const file: File | null = data.get('file') as unknown as File;
-
-  if(!file) return "fail"
-
-  const fileName = file.name;
-  const key = data.get('key') as string;
-
-  // Convert the file to a buffer
-  const bytes = await file.arrayBuffer();
-  const buffer = Buffer.from(bytes);
-
-  // Get the blake3 sum of the key entered
-  const mainKey = await blake3(key);
-  const encKey = Buffer.from(mainKey, 'hex');
-
-  // // Encrypt the buffer
-  const iv = crypto.randomBytes(16);
-  const cipher = crypto.createCipheriv('aes-256-cbc', encKey, iv);
-  const encrypted = Buffer.concat([cipher.update(buffer), cipher.final()]);
-  const encryptedFileBuffer = Buffer.concat([iv, encrypted]);
-
-  // Upload the encrypted file to discord
-  const res = await addDataDB(encryptedFileBuffer, fileName, key);
-
-  if(!res) return "fail"
-  else return {status: "success", fileid: res}
-}
+  const response = await fetch(`${process.env.DB_BOT_URL}/api/download`, {
+    method: 'POST',
+    body: dataToSend
+  })
 
 
-export async function downloadFile(data: FormData) {
-  const fileID = data.get("fileid") as string;
-  const key = data.get("key") as string;
+  if(!response.ok) return "fail";
 
-  const file_data = await getOriginalFileURL(fileID, key);
-  if(file_data === "fail") return "fail";
+  const file_metadata = await response.json();
 
-  const fileContent = await fetch(file_data.url);
+  const fileContent = await fetch(file_metadata.url);
 
   if(!fileContent.ok) return "fail";
 
   const arrayBuffer = await fileContent.arrayBuffer();
   const buffer = Buffer.from(arrayBuffer);
 
-  const mainKey = await blake3(key);
+  const mainKey = await blake3(key_entered);
   const decKey = Buffer.from(mainKey, 'hex');
 
   // Decrypt the file
@@ -190,5 +123,5 @@ export async function downloadFile(data: FormData) {
     decipher.final()
   ]);
 
-  return {file_info: file_data, decData: decrypted};
+  return {file_info: file_metadata, decData: decrypted};
 }
--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,5 @@
 module.exports = {
-  webpack: (config: any, options: any) => {
-      config.module.rules.push({
-        test: /\.node/,
-        use: 'node-loader'
-      })
-   
-      return config
-    },
+
 };
 
 // export default nextConfig;
